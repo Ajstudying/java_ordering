a231eb2805c3b4dcdeb8db62aad130bd17d75810
Ajstudying <rkddowls1@gmail.com>
2025-01-20 20:29:50 +0900

    ãitemList getMapping 생성

--- src/main/java/ordering_program/item/ItemController.java
+++ src/main/java/ordering_program/item/ItemController.java
@@ -1,10 +1,22 @@
 package ordering_program.item;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/items")
 public class ItemController {
+    @Autowired
+    ItemRepository itemRepo;
+
+    @GetMapping(value = "/list")
+    public List<Item> getItemLists() {
+        return itemRepo.findItemSortById();
+    }
 }
--- src/main/java/ordering_program/item/ItemRepository.java
+++ src/main/java/ordering_program/item/ItemRepository.java
@@ -1,6 +1,7 @@
 package ordering_program.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,5 +9,9 @@ import java.util.Optional;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Query("select b from Item b order by b.id asc")
+    List<Item> findItemSortById();
+
 
 }
